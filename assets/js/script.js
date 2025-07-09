d583c1b0f38bfce0670100913bc9381809b49bee
vguima404 <victorribeiro4004@gmail.com>
2025-07-09 12:45:20 -0300

    feat: adicionado a função de iterar a pontuação do jogador ao clicar no quadrado certo

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ const state = {
     squares: document.querySelectorAll(".square"),
     score: document.querySelector('#score'),
     timeLeft: document.querySelector('#time-left'),
-    enemy: document.querySelector('.enemy')
+    enemy: document.querySelector('.enemy'),
   },
   values: {
     timerId: null,
     interval: 1000,
+    hitPosition: 0,
+    result: 0,
   },
 }
 
@@ -19,15 +21,28 @@ function randomSquare () {
   state.view.squares.forEach((square) => {
     square.classList.remove('enemy');
   });
-
+  
   let randomNumber = Math.floor(Math.random() * 9);
   let randomSquare = state.view.squares[randomNumber];
+  state.values.hitPosition = randomSquare.id;
   randomSquare.classList.add('enemy')
 }
 
+function addListenerHitBox(){
+  state.view.squares.forEach((square) => {
+    square.addEventListener('mousedown', () =>{
+      if (square.id === state.values.hitPosition){
+        state.values.result++
+        state.view.score.textContent = state.values.result
+        state.values.hitPosition = null
+      }
+    })
+  })
+}
 
 function init(){
   moveEnemy();
+  addListenerHitBox();
 }
 
 init();
