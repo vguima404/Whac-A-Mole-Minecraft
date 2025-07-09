8c2df88977706bfff0382e3864a827a08c4da5a5
vguima404 <victorribeiro4004@gmail.com>
2025-07-08 22:36:43 -0300

    feat: adicionado a function de sortear um quadrado para aparecer o creeper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 const state = {
   view: {
-    
+    squares: document.querySelectorAll(".square"),
+    score: document.querySelector('#score'),
+    timeLeft: document.querySelector('#time-left'),
+    enemy: document.querySelector('.enemy')
   },
   values: {
 
   },
 }
 
+function randomSquare () {
+  state.view.squares.forEach((square) => {
+    square.classList.remove('enemy');
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add('enemy')
+}
+
+
+function init(){
+  randomSquare();
+}
+
+init();
